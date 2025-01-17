eeaca1a0fa7be14a5baa1f89b08e8ba175fc88a3
Oleksandr Drozhcha <idroidodro@gmail.com>
2025-01-17 23:14:52 +0100

    working on html/css part

---
+++ js/camping-philosophy.js
@@ -0,0 +1,33 @@
+// window.addEventListener("load", function () {
+//   const section = document.querySelector(".camping-philosophy");
+//   const divContainer = document.createElement("div");
+//   const h2 = document.createElement("h2");
+//   const topSpan = document.createElement("span");
+//   const bottomSpan = document.createElement("span");
+//   const mediaContainer = document.createElement("div");
+//   const followUs = document.createElement("p");
+//   const facebookDiv = document.createElement("div");
+//   const InstagramDiv = document.createElement("div");
+//   const YoutubeDiv = document.createElement("div");
+
+//   divContainer.classList.add(".camping-philosophy--container");
+//   const spans = this.document.querySelectorAll("span");
+//   spans.forEach((span) => span.classList.add(".camping-philosophy--text"));
+
+//   h2.textContent = "Camping is Our Philosophy";
+//   topSpan.textContent =
+//     "A camping tour is a great way to spend an active weekend with your beloved or family. We offer carefully designed, guided tours to the most interesting natural parks in the USA and Canada.";
+//   bottomSpan.textContent =
+//     "Here you can enjoy the clean air, rock climbing, hiking, cycling and camping with all the necessary amenities. We will show you the most beautiful places and introduce you to the history of national parks. Join us on our next adventure!";
+
+//   followUs.textContent = "FollowUs";
+//   section.append(divContainer);
+//   divContainer.append(h2);
+//   divContainer.append(topSpan);
+//   divContainer.append(bottomSpan);
+//   divContainer.append(mediaContainer);
+//   mediaContainer.append(followUs);
+//   mediaContainer.append(facebookDiv);
+//   mediaContainer.append(InstagramDiv);
+//   mediaContainer.append(YoutubeDiv);
+// });

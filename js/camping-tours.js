edf3cc36b483003b0791c340a9e18f9ff03534e0
Oleksandr Drozhcha <idroidodro@gmail.com>
2025-01-21 20:18:55 +0100

    working on burger menu

--- js/camping-tours.js
+++ js/camping-tours.js
@@ -1,38 +1,50 @@
-const mediaQuery = window.matchMedia('(min-width: 460px)');
+const mediaQuery460 = window.matchMedia("(min-width: 460px)");
+const mediaQuery917 = window.matchMedia("(min-width: 917px)");
 
-const campingTours = document.querySelector('.camping_tours')
-const campingToursH2 = document.querySelector('.camping_tours-h2');
-const campingToursContrainer = document.querySelector('.camping_tours--container');
-const items = document.querySelectorAll('.camping_tours--item');
+const campingTours = document.querySelector(".camping_tours");
+const campingToursH2 = document.querySelector(".camping_tours-h2");
+const campingToursContrainer = document.querySelector(
+  ".camping_tours--container"
+);
+
+const items = document.querySelectorAll(".camping_tours--item");
 
 const imgs = [
-    "/img/index/camping-tours/index.yellowstone-tetons.jpeg",
-    "/img/index/camping-tours/index.utah_arizona.jpeg",
-    "/img/index/camping-tours/index.montana.jpeg",
-    "/img/index/camping-tours/index.banff_yoho.jpeg"
+  "/img/index/camping-tours/index.yellowstone-tetons.jpeg",
+  "/img/index/camping-tours/index.utah_arizona.jpeg",
+  "/img/index/camping-tours/index.montana.jpeg",
+  "/img/index/camping-tours/index.banff_yoho.jpeg",
 ];
 
-items.forEach( (item, index) => {
-    const img = document.createElement('img');
-    img.src = imgs[index];
-    img.alt = `camping-tours picture${index}`;
-    item.height = "100%";
-    img.style.objectFit = 'cover';    
-    item.append(img);
+items.forEach((item, index) => {
+  const img = document.createElement("img");
+  img.src = imgs[index];
+  img.alt = `camping-tours picture${index}`;
+  img.className = `tour${index + 1}`;
+  img.className = 'img-tours';
+  item.height = "100%";
+  img.style.objectFit = "cover";
+  item.append(img);
 });
 
-campingTours.style.display = 'grid';
-campingTours.style.justifyItems = 'center';
-campingTours.style.gap = '20px';
-campingToursContrainer.style.display = 'grid';
+campingTours.style.display = "grid";
+campingTours.style.justifyItems = "center";
+campingTours.style.gap = "20px";
+campingToursContrainer.style.display = "grid";
 
-mediaQuery.addEventListener('change', (event) => {
-    if (event.matches) {
-        campingToursContrainer.style.gridTemplateColumns = '1fr 1fr'
-} else {
-    campingToursContrainer.style.gridTemplateColumns = '1fr'
+if (window.matchMedia("(min-width: 460px)").matches) {
+  campingToursContrainer.style.gridTemplateColumns = "1fr 1fr";
+
+} else if (window.matchMedia("(min-width: 917px)").matches){
+      tour2.style.height = "420px";
 }
-});
+  mediaQuery460.addEventListener("change", (event) => {
+    if (event.matches) {
+      campingToursContrainer.style.gridTemplateColumns = "1fr 1fr";
+        tour2.style.height = "280px";
+    } else {
+      campingToursContrainer.style.gridTemplateColumns = "1fr";
+    }
+  });
 
-campingToursContrainer.style.gap = '15px';
-
+campingToursContrainer.style.gap = "15px";

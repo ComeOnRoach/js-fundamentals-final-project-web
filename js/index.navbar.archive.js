dafcefc8e2aabefe418d387a78c6af6b5f69b720
Oleksandr Drozhcha <idroidodro@gmail.com>
2025-01-22 19:24:58 +0100

    burger menu upd

---
+++ js/index.navbar.archive.js
@@ -0,0 +1,51 @@
+// const body = document.querySelector("body");
+// const header = document.querySelector(".header");
+// const btnSub = document.querySelector(".header__menu-btn");
+// const navLinks = document.querySelector(".header__nav-menu");
+// const headerContainerNav1110 = document.querySelector(
+//   ".header__container-nav1110"
+// );
+// const headerContainerNav320 = document.querySelector(
+//   ".header__container-nav320"
+// );
+
+// btnSub.addEventListener("click", () => {
+//   body.classList.toggle("overflow-hidden");
+//   header.classList.toggle("nav-visible");
+// });
+
+// navLinks.addEventListener("click", () => {
+//   body.classList.remove("overflow-hidden");
+//   header.classList.remove("nav-visible");
+// });
+
+// const handleResize = () => {
+//   if (window.matchMedia("(min-width: 1110px)").matches) {
+
+//   }
+//   if (window.matchMedia("(min-width: 320px)").matches) {
+
+//   }
+// };
+
+// // window.addEventListener("resize", handleResize);
+
+// // handleResize();
+
+// // const handleResize = () => {
+// //   if (window.matchMedia("(min-width: 1110px)").matches) {
+// //     const headerСontainerNav = document.querySelector(".header__container nav");
+// //     if (headerСontainerNav) {body.prepend(headerСontainerNav)};
+// //   }
+
+// //   if(window.matchMedia("(min-width: 320px)").matches) {
+// //     const bodyСontainerNav = document.querySelector("body.header__container-nav");
+// //     if (bodyСontainerNav) {
+// //       headerСontainer.prepend(bodyСontainerNav);
+// //     }
+// //   }
+// // };
+
+// // window.addEventListener("resize", handleResize);
+
+// // handleResize();
